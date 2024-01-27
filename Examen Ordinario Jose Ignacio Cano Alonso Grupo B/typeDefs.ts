51511aa18db9ac4886cf0db4aaab9c6b4f445b70
NachoCano123 <145673920+NachoCano123@users.noreply.github.com>
2024-01-27 18:46:35 +0100

    Add files via upload

---
+++ Examen Ordinario Jose Ignacio Cano Alonso Grupo B/typeDefs.ts
@@ -0,0 +1,21 @@
+export const typeDefs = `#graphql
+
+type Contact {
+    id:ID!
+    Nombre_completo: String!
+    Telefono: String!
+    Pais: String!
+    Hora_actual: String!
+}
+
+type Query{
+    getContact(id:ID!):Contact!
+    getContacts(id:ID!):[Contact!]!
+}
+
+type Mutation{
+    addContact(Nombre_completo: String!, Telefono: String!): Contact!
+    deleteContact(id:ID!):Contact!
+    #updateContact
+}
+`
